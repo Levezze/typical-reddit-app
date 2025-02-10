88136d781f08c8d181501cff4862da598953e3b1
Levezze <lev.zhi@gmail.com>
2025-02-10 03:04:53 -0500

    Add mobile styling for contact button

--- src/app/components/ContactButton/ContactButton.tsx
+++ src/app/components/ContactButton/ContactButton.tsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 type ContactButtonProps = {
   text: string;
 };
 
 export const ContactButton = ({ text } : ContactButtonProps) => {
+  const [hover, setHover] = useState('');
   return (
     <Link 
-      className="contact-button"
+      className={`contact-button ${hover}`}
       to="/contact"
+      onMouseOver={()=>setHover('hover')}
+      onMouseOut={()=>setHover('')}
+      onTouchEnd={()=>setTimeout(()=>setHover(''), 300)}
     >
       {text.toUpperCase()}
     </Link>
