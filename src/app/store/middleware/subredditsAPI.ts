fddce70d4ffdd968466ff85593a3edb3842bfb90
Levezze <lev.zhi@gmail.com>
2025-02-11 17:14:13 -0500

    Fix: Add authorization header to subreddits requests

--- src/app/store/middleware/subredditsAPI.ts
+++ src/app/store/middleware/subredditsAPI.ts
@@ -1,10 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { ResponseData } from "../../../types/api";
+import { RootState } from "../store";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const subredditsApi = createApi({
   reducerPath: 'subredditsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: API_BASE_URL }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: API_BASE_URL,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     getPopularSubreddits: builder.query<ResponseData, void>({
       query: () => 'subreddits/popular',
